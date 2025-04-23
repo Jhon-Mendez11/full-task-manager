bdfed28199f495652acc46357d2bb182a42bf9c0
jhon-mendez11 <jhon.danier2000@gmail.com>
2025-04-23 18:14:15 -0500

    Se agrego el inicio de sesión

--- js/app.js
+++ js/app.js
@@ -1,38 +1,75 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const taskForm = document.getElementById('task-form');
+    //const taskForm = document.getElementById('task-form');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
     let tasks = [];
     let isEditing = false;
     let editingId = null;
-
-    taskForm.addEventListener('click', (e) => {
-        const vti = taskInput.value.trim();
-        if (vti !== '') {
-            if (isEditing) {
-                tasks = tasks.map(task =>
-                    task.id == editingId ? {
-                        ...task, text: vti
-                    } : task);
-                isEditing = false;
-                editingId = null;
-                taskForm.innerText = "Agregar";
+    /*
+        taskForm.addEventListener('click', (e) => {
+            const vti = taskInput.value.trim();
+            if (vti !== '') {
+                if (isEditing) {
+                    tasks = tasks.map(task =>
+                        task.id == editingId ? {
+                            ...task, text: vti
+                        } : task);
+                    isEditing = false;
+                    editingId = null;
+                    taskForm.innerText = "Agregar";
+                }
+                else {
+                    const task = {
+                        id: Date.now(),
+                        text: vti,
+                        complete: false
+                    };
+                    tasks.push(task);
+                    console.log(tasks);
+                }
+                renderTasks();
+                taskInput.value = '';
             }
-            else {
-                const task = {
-                    id: Date.now(),
-                    text: vti,
-                    complete: false
-                };
-                tasks.push(task);
-                console.log(tasks);
-            }
-            renderTasks();
-            taskInput.value = '';
-        }
-    });
-
+        });
+    */
     function renderTasks() {
+        console.log("Runing");
+
+        fetch('server/user/session_info.php')
+            .then(res => res.json())
+            .then(data => {
+                if (data.user_id) {
+                    console.log('Sesión activa para usuario:', data.user_id);
+                    fetch('server/task/index.php?user_id=' + user_id)
+                        .then(response => response.json())
+                        .then(tks => {
+                            console.log(tks);
+                            tks.forEach(task => {
+                                const li = document.createElement('li');
+                                li.classList.toggle('completed', task.completed); // Agrega la clase solo si está completa
+
+                                let buttons =
+                                    '<button class="complete-btn" onclick="completeTask(' + task.id + ')">' +
+                                    (task.completed ? 'Deshacer' : 'Completar') + '</button>';
+
+                                if (!task.completed) {
+                                    buttons +=
+                                        '<button class="edit-btn" onclick="editTask(' + task.id + ')">Editar</button>' +
+                                        '<button class="delete-btn" onclick="deleteTask(' + task.id + ')">Eliminar</button>';
+                                }
+
+                                li.innerHTML = '<span>' + task.title + '</span><div>' + buttons + '</div>';
+                                taskList.appendChild(li);
+                            }
+                            );
+                        });
+                } else {
+                    console.warn(data.error);
+                    window.location.href = 'login.php';
+                }
+            });
+
+
         taskList.innerHTML = '';
         tasks.forEach(task => {
             const li = document.createElement('li');
@@ -75,4 +112,5 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
 
+    renderTasks();
 });
